cb61e6424bc3d4abeb8d38040df6cd7cd9181b5c
BlackShirt-Coder <haruta685@gmail.com>
2023-11-01 00:18:44 +0630

    first_react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Body from "./components/Body";
+
+function App() {
+    return (
+        <div>
+            <Header></Header>
+
+            <hr/>
+            <Body></Body>
+            <hr/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,6 @@
+function Body(){
+    return(
+        <h1>Body Body</h1>
+    )
+}
+export default  Body
