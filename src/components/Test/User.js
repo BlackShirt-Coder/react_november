ac70e98781dbd982de38066b4e09bf38a6434e9d
BlackShirt-Coder <haruta685@gmail.com>
2023-11-01 01:40:02 +0630

    css_styling

---
+++ src/components/Test/User.js
@@ -0,0 +1,16 @@
+import "./User.css";
+
+function User() {
+    return (
+        <div>
+            <div>
+                <h1>User One</h1>
+            </div>
+            <div>
+                <h2>User Two</h2>
+            </div>
+        </div>
+    )
+}
+
+export default User;
