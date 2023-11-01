974f5bb5097f1865f00f0e769fa8d8159575c8f8
BlackShirt-Coder <haruta685@gmail.com>
2023-11-01 22:16:36 +0630

    useState_intro

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import React from "react";
+import React ,{useState} from "react";
+
 import Users from "./components/Users/Users";
+import "./style.css";
+import user from "./components/Test/User";
+
+
 
 function App() {
-    let users=[
+
+    let users = [
         {
-            id:1,
-            user:"mgmg",
-            image:"https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png",
-            phone:"09302902930"
+            id: 1,
+            user: "mgmg",
+            image: "https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png",
+            phone: "09302902930"
         },
         {
-            id:2,
-            user:"aung aung",
-            image:"https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png",
-            phone:"09859483943"
+            id: 2,
+            user: "aung aung",
+            image: "https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png",
+            phone: "09859483943"
         },
         {
-            id:3,
-            user:"su su",
-            image:"https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png",
-            phone:"09883299232"
+            id: 3,
+            user: "su su",
+            image: "https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png",
+            phone: "09883299232"
         }
     ];
+    let [count,setCount]=useState(0);
     return (
         <div>
+            <div className="p-4">
+                <p>{count}</p>
+                <button className="btn btn-info "  onClick={()=>setCount(++count)}><i className="fa fa-edit"></i></button>
+            </div>
             {
-                users.map(usr=><Users user={usr} key={usr.id}></Users>)
+                users.map(usr => <Users user={usr} key={usr.id}></Users>)
             }
 
         </div>
