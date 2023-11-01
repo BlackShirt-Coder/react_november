c47d7adec875c30118f9d20b166247df7eede992
BlackShirt-Coder <haruta685@gmail.com>
2023-11-01 21:10:18 +0630

    dynamic_props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from "react";
-import Usersr from "./components/Users/Users";
 import Users from "./components/Users/Users";
+
 function App() {
-    let img="https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png";
+    let users=[
+        {
+            id:1,
+            user:"mgmg",
+            image:"https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png",
+            phone:"09302902930"
+        },
+        {
+            id:2,
+            user:"aung aung",
+            image:"https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png",
+            phone:"09859483943"
+        },
+        {
+            id:3,
+            user:"su su",
+            image:"https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png",
+            phone:"09883299232"
+        }
+    ];
     return (
         <div>
-
-
-          <Users image={img} phone="0939209032"></Users>
-          <Users image={img} phone="0948484432"></Users>
+            {
+                users.map(usr=><Users user={usr} key={usr.id}></Users>)
+            }
 
         </div>
+
     )
 }
 
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,19 +1,23 @@
 import React from "react";
-function Users({image,phone}){
+function Users(obj){
     return(
         <div className="container my-5 card py-3">
         <div className="row">
             <div className="col-2">
-                <img src={image} alt=""  width="100px" height="100px"  />
+                <img src={obj.user.image} alt=""  width="100px" height="100px"  />
             </div>
             <div className="col-7 text-center ">
+                <div style={{display:"inline-flex"}}>
+                    <p>User Name&nbsp;</p>
+                    <p><strong>{obj.user.user}</strong></p>
+                </div>
                 <p>Phone Number</p>
-                <p><strong>{phone}</strong></p>
+                <p><strong>{obj.user.phone}</strong></p>
             </div>
             <div className="col-3">
                 <button className="btn btn-danger"><i className="fa fa-trash"></i></button>
             </div>
-            <div><h2>Tesing</h2></div>
+
         </div>
         </div>
     );
